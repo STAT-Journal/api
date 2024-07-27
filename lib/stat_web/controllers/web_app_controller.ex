2b1a1c08bf5a8e2cdf025ce9636d88e732a4256e
ehharvey <eharvey3704@conestogac.on.ca>
2024-07-27 14:28:51 -0400

    * Tweak app path

--- lib/stat_web/controllers/web_app_controller.ex
+++ lib/stat_web/controllers/web_app_controller.ex
@@ -1,11 +1,8 @@
 defmodule StatWeb.WebAppController do
   use StatWeb, :controller
-
-  @index_path Application.app_dir(:stat, "priv/static/webapp")
-
   def index(conn, _params) do
     conn
     |> put_resp_content_type("text/html")
-    |> send_file(200, @index_path <> "/index.html")
+    |> send_file(200, Application.app_dir(:stat, "priv/static/webapp/index.html"))
   end
 end
